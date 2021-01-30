c37b989bcfa2e3a519db589e35f90fb4e3f66328
Deepak_Verma <deepakverma.dv0598@gmail.com>
2021-01-30 17:57:54 +0530

    Maven12345

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace dotnet_core_tutorial
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World! Your Welcome My friend on the Core .Net");
+                    await context.Response.WriteAsync("Hello World!  Welcome My friend on the Core .Net");
                 });
             });
         }
